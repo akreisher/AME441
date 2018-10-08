850556b0695c535d8b2d0719ba0185030b548919
unknown <kreisher@usc.edu>
2018-10-08 12:11:06 -0700

    Add filter test/parser

--- filterTest.cpp
+++ filterTest.cpp
@@ -3,17 +3,21 @@
 #include <algorithm>
 #include <cmath>
 #include "AHRS.h"
+#include <wiringPi.h>
 
 #define pi 3.14159265359
 #define dt 0.01
 
 void compFilter(float gyroData[3],float accData[3], float* pitch, float* roll,float alpha)
 {
+	//addnew gyro values
 	*pitch += (gyroData[0]*dt);
 	*roll += (gyroData[1]*dt);
 
+	//check for garbage data
 	int magForce = abs(accData[0])+abs(accData[1])+abs(accData[2]);
 	if (magForce>1&&magForce<2){
+		//if good data, add acceleration data
 		pitchAcc = atan2f(accData[1],accData[2])*180/pi;
 		*pitch = (*pitch)*(1-alpha) + alpha*pitchAcc;
 		rollAcc = atan2f(accData[0],accData[2])*180/pi;
@@ -24,19 +28,27 @@ void compFilter(float gyroData[3],float accData[3], float* pitch, float* roll,fl
 
 int main()
 {
-	AHRS com = AHRS("/dev/ttyACM0");
-	std::ofstream ofile;
-	ofile.open("filterData.txt")
+	AHRS com = AHRS("/dev/ttyACM0");//open IMU
+	wiringPiSetupSys();
+	//open ofiles
+	std::ofstream pitchfile;
+	std::ofstream rollfile;
+	ofile.open("pitchData.txt");
+	ofile.open("rollData.txt");
 	printf("Initializing\n\n");
+	//get initial attitude
 	float initPitch = com.GetPitch();
 	float initRoll = com.GetRoll();
 	float roll[10];
 	float pitch[10];
+	//set initial angles
 	for (int i = 0;i<10;i++){
 		roll[i]=initRoll;
 		pitch[i]=initPitch;
 	}
+	//1000 steps
 	for (int i = 0;i<1000;i++){
+		//get sensor dat
 		float accData[3];
 		accData[0] = com.GetWorldLinearAccelX();
 		accData[1] = com.GetWorldLinearAccelY();
@@ -47,15 +59,20 @@ int main()
 		gyroData[1]=com.GetRoll();
 		gyroData[2]=com.GetYaw();
 		long time = com.GetLastSensorTimestamp();
-		ofile<<time<<","<<0<<",";
-		ofile<<gyroData[0]<<","<<gyroData[1];
+
+		//print to files
+		pitchFile<<time<<","<<gyroData[0];
+		rollFile<<time<<","<<gyroData[1];
 		for (int j = 0;j<10;j++)
 		{
+			//apply filter
 			compFilter(gyroData,accData,&pitch[i],&roll[i],0.2+0.2*i);
-			ofile<<","<<0.2+i*0.2;
-			ofile<<","<<pitch[i]<<","<<roll[i]
+			pitchFile<<","<<pitch[i];
+			rollFile<<roll[i];
 		}
-		ofile<<std::endl;
+		pitchFile<<std::endl;
+		rollFile<<std::endl;
+		delay(10);
 	}
 
 }
